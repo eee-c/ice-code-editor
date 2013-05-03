e84118c948d944997791773f8b5c4c46d5cd5b95
Chris Strom <eee.c@eeecooks.com>
2013-05-03 00:37:28 -0400

    First pass at hiding JS dependencies from implementation
    Create script tags dynamically inside the constructor, pulling the ACE JavaScript libraries
    from the relative package path.
    
    Don't create an instance of the ACE editor until the dynamically created script has
    loaded.
    
    Add a guard in the content setter to prevent calling methods on a JS proxy that might not
    be up yet.
    
    Huge thanks to @santiaago for pairing on this.

--- example/public/main.dart
+++ example/public/main.dart
@@ -1,7 +1,8 @@
 import 'package:ice_code_editor/editor.dart' as ICE;
 
 main() {
-  var ice = new ICE.Editor('ace');
+  var ice = new ICE.Editor('ice');
+
   ice.content = '''
 main() {
   print("Dart rocks");
--- lib/editor.dart
+++ lib/editor.dart
@@ -1,6 +1,7 @@
 library ice;
 
 import 'dart:html';
+import 'dart:async';
 import 'package:js/js.dart' as js;
 
 class Editor {
@@ -8,12 +9,28 @@ class Editor {
   String title;
   var _ace;
 
-  Editor(el, {this.edit_only:false, this.autoupdate:true, this.title}) {
-    _ace = js.context.ace.edit(el);
+  Editor(id, {this.edit_only:false, this.autoupdate:true, this.title}) {
+    var script = new ScriptElement();
+    script.src = "packages/ice_code_editor/ace/ace.js";
+    document.head.nodes.add(script);
+
+    script.onLoad.listen((event) {
+      this._ace = js.context.ace.edit(id);
+      js.retain(this._ace);
+    });
   }
 
   set content(String data) {
-    _ace.setValue(data, -1);
+    if (this._ace == null) {
+      // TODO: completer?
+      // this.waitForAce.then(updateAceValue);
+      var wait = new Duration(milliseconds: 50);
+      new Timer(wait, (){ this.content = data;});
+      return;
+    }
+
+    this._ace.setValue(data, -1);
+    this._ace.focus();
   }
 
   String get content => _ace.getValue();
