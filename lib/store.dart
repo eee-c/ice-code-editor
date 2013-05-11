bbcb14fb915443d3e755a9d397a1fbd2d5d3801b
Chris Strom <github@eeecomputes.com>
2013-05-11 11:45:20 -0400

    First pass at localStorage for ICE projects
    Something of a pain because it needs to be ordered (LIFO), need to be
    able to lookup by key (title), needs to map into JSON for localStorage. So we're
    serializing to a JSON list of HashMap, where the title is in the HashMap, but
    also serves as the lookup key.
    
    So far, we have insert and update working.
    
    Paired with @korishev

--- lib/store.dart
+++ lib/store.dart
@@ -1,9 +1,17 @@
 library ice;
 
+import 'dart:html';
+import 'dart:collection';
 import 'dart:crypto';
+import 'dart:json' as JSON;
 import 'package:js/js.dart' as js;
 
-class Store {
+class Store implements HashMap<String, HashMap> {
+  const String codeEditor = 'codeeditor';
+  List _projects;
+
+  Store() { }
+
   static String encode(String string) {
     var gzip = js.context.RawDeflate.deflate(string);
     return CryptoUtils.bytesToBase64(gzip.codeUnits);
@@ -12,5 +20,48 @@ class Store {
     var bytes = CryptoUtils.base64StringToBytes(string);
     var gzip = new String.fromCharCodes(bytes);
     return js.context.RawDeflate.inflate(gzip);
+  }
+
+  int get length => projects.length;
+
+  void operator []=(String key, Object data) {
+    data['title'] = key;
+
+    _updateAtIndex(_indexOfKey(key), data);
+
+    _sync();
+  }
+
+  int _indexOfKey(String key) => projects.indexOf(this[key]);
+
+  void _updateAtIndex(i, data) {
+    if (i == -1) {
+      projects.insert(0, data);
+    }
+    else {
+      projects[i] = data;
+    }
+  }
+
+  HashMap operator [](String key) {
+    return projects.
+      firstWhere(
+        (p) => p['title'] == key,
+        orElse: () => null
+      );
+  }
+
+  List get projects {
+    if (_projects != null) return _projects;
+
+    var json = window.localStorage[codeEditor];
+    return _projects = (json == null) ? [] : JSON.parse(json);
+  }
+
+  void refresh() => _projects = null;
+
+
+  void _sync() {
+    window.localStorage[codeEditor] = JSON.stringify(projects);
   }
 }
--- test/store_test.dart
+++ test/store_test.dart
@@ -12,4 +12,57 @@ main() {
       expect(Store.decode("88gvT6nUUXDKT1IEAA=="), equals("Howdy, Bob!"));
     });
   });
+
+  group("store/retrieve", (){
+    var it;
+    setUp(()=> it = new Store());
+
+    test("it can store data", (){
+      it['foo'] = {'bar': 42};
+      expect(it['foo']['bar'], equals(42));
+    });
+
+    test("it can store multiple records", (){
+      it['one'] = {'bar': 1};
+      it['two'] = {'bar': 2};
+      it['foo'] = {'bar': 42};
+      expect(it['foo']['bar'], equals(42));
+    });
+
+    test("it can retrive arbitrary records", (){
+      it['one'] = {'bar': 1};
+      it['two'] = {'bar': 2};
+      it['foo'] = {'bar': 42};
+      expect(it['one']['bar'], equals(1));
+    });
+
+    test("it can overwrite existing records", (){
+      it['one'] = {'bar': 1};
+      it['two'] = {'bar': 2};
+      it['foo'] = {'bar': 42};
+      it['two'] = {'bar': 7};
+      expect(it['two']['bar'], equals(7));
+    });
+
+    test("it does not create new records when overwriting old ones", (){
+      it['one'] = {'bar': 1};
+      it['two'] = {'bar': 2};
+      it['foo'] = {'bar': 42};
+      it['two'] = {'bar': 7};
+      expect(it.length, equals(3));
+    });
+  });
+
+  group("store/retrieve", (){
+    var it;
+    setUp(()=> it = new Store());
+
+    test("records persist", (){
+      it['foo'] = {'bar': 42};
+      it.refresh();
+
+      expect(it['foo']['bar'], equals(42));
+    });
+  });
+
 }
