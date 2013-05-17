6fe7e078636395b420b901705cf714c069bc430a
Chris Strom <github@eeecomputes.com>
2013-05-17 00:50:30 -0400

    Start on the full-screen menu
    Have the sharing dialog partly done.
    
    Pairing with @batasrki

--- lib/full.dart
+++ lib/full.dart
@@ -8,6 +8,7 @@ class Full {
     el = new Element.html('<div id=ice>');
     document.body.nodes.add(el);
 
+    _attachToolbar();
     _fullScreenStyles();
     _ice = new Editor('#ice', enable_javascript_mode: enable_javascript_mode);
 
@@ -16,6 +17,81 @@ class Full {
 
   Future get editorReady => _ice.editorReady;
 
+  _attachToolbar() {
+    var toolbar = new Element.html('<div class=ice-toolbar>');
+    toolbar.style
+      ..position = 'absolute'
+      ..top = '10px'
+      ..right = '20px'
+      ..zIndex = '999';
+
+    _attachMenuButton(toolbar);
+
+    el.children.add(toolbar);
+  }
+
+  _attachMenuButton(parent) {
+    var el = new Element.html('<button>☰</button>');
+    parent.children.add(el);
+
+    el.onClick.listen((e)=> this.toggleMenu());
+  }
+
+  toggleMenu() {
+    if (queryAll('.ice-menu').isEmpty) _showMenu();
+    else _hideMenu();
+  }
+
+  _showMenu() {
+    var menu = new Element.html('<ul class=ice-menu>');
+    el.children.add(menu);
+
+    menu.style
+      ..position = 'absolute'
+      ..right = '17px'
+      ..top = '55px'
+      ..zIndex = '999';
+
+    menu.children
+      ..add(new Element.html('<li>New</li>'))
+      ..add(new Element.html('<li>Open</li>'))
+      ..add(new Element.html('<li>Save</li>'))
+      ..add(new Element.html('<li>Make a Copy</li>'))
+      ..add(_shareMenuItem())
+      ..add(new Element.html('<li>Download</li>'))
+      ..add(new Element.html('<li>Help</li>'));
+  }
+
+  _hideMenu() {
+    query('.ice-menu').remove();
+  }
+
+  _shareMenuItem() {
+    return new Element.html('<li>Share</li>')
+      ..onClick.listen((e) => _openShareDialog());
+  }
+
+  _openShareDialog() {
+    var dialog = new Element.html(
+        '''
+        <div class=ice-dialog>
+        <h1>Copy this link to share your creation:</h1>
+        <input
+          value="http://gamingjs.com/ice/#B/${encodedContent}"
+          style="width=400px; padding=5px; border=0px">
+        </div>
+        '''
+    );
+
+    el.children.add(dialog);
+
+    dialog.style
+      ..left = "${(window.innerWidth - dialog.offsetWidth)/2}px"
+      ..top = "${(window.innerHeight - dialog.offsetHeight)/2}px";
+  }
+
+  String get encodedContent => Gzip.encode(_ice.content);
+
   _fullScreenStyles() {
     document.body.style
       ..margin = '0px'
@@ -23,7 +99,6 @@ class Full {
   }
 
   _applyStyles() {
-
      var editor_el = el.query('.ice-code-editor-editor');
 
      editor_el.style
--- test/full_test.dart
+++ test/full_test.dart
@@ -2,6 +2,8 @@ part of ice_tests;
 
 full_tests() {
   group("initial UI", (){
+    tearDown(()=> document.query('#ice').remove());
+
     test("the editor is full-screen", (){
       var it = new Full(enable_javascript_mode: false);
 
@@ -15,5 +17,63 @@ full_tests() {
     });
 
     //test("has a default project", (){});
+  });
+
+  group("main toolbar", (){
+    setUp(()=> new Full(enable_javascript_mode: false));
+    tearDown(()=> document.query('#ice').remove());
+
+    test("it has a menu button", (){
+      var buttons = document.queryAll('button');
+      expect(buttons.any((e)=> e.text=='☰'), isTrue);
+    });
+
+    test("clicking the menu button brings up the menu", (){
+      var menu_button = queryAll('button').
+        firstWhere((e)=> e.text=='☰');
+
+      menu_button.click();
+      var menu = queryAll('li').
+        firstWhere((e)=> e.text.contains('Help'));
+
+      expect(menu, isNotNull);
+    });
+
+    test("clicking the menu button a second time hides the menu", (){
+      var menu_button = queryAll('button').
+        firstWhere((e)=> e.text=='☰');
+
+      menu_button.click();
+
+      expect(queryAll('li').map((e)=> e.text), contains('Help'));
+
+      menu_button.click();
+
+      expect(queryAll('li').map((e)=> e.text), isEmpty);
+    });
+  });
+
+  group("sharing", (){
+    setUp(()=> new Full(enable_javascript_mode: false));
+    tearDown(()=> document.query('#ice').remove());
+
+    test("clicking the share link shows the share dialog", (){
+      queryAll('button').
+        firstWhere((e)=> e.text=='☰').
+        click();
+
+      queryAll('li').
+        firstWhere((e)=> e.text=='Share').
+        click();
+
+      expect(
+        queryAll('div').map((e)=> e.text).toList(),
+        contains(matches('Copy this link'))
+      );
+    });
+
+    test("there is a modal overlay behind the dialog", (){});
+    test("share link is encoded", (){});
+    test("menu should close when share dialog activates", (){});
   });
 }
