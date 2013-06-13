aa3b486507342b677fbebc553d36dea621c231e1
Chris Strom <github@eeecomputes.com>
2013-06-13 19:22:20 -0400

    Test HTML5 rendering of the full screen editor
    Fixes #19

---
+++ test/html5_test.dart
@@ -0,0 +1,41 @@
+library ice_html5_test;
+
+import 'package:unittest/unittest.dart';
+import 'dart:html';
+import 'dart:async';
+
+import 'helpers.dart' as helpers;
+import 'package:ice_code_editor/ice.dart';
+
+main() {
+  group("HTML5", (){
+    group("Full Screen ICE", (){
+      tearDown(()=> document.query('#ice').remove());
+
+      test("the editor is full-screen", (){
+        var it = new Full(enable_javascript_mode: false);
+
+        _test(_) {
+          var el = document.query('#ice');
+          var editor_el = el.query('.ice-code-editor-editor');
+          expect(editor_el.clientWidth, window.innerWidth);
+          expect(editor_el.clientHeight, closeTo(window.innerHeight,1.0));
+          expect(document.body.clientHeight, greaterThan(10));
+        };
+        it.editorReady.then(expectAsync1(_test));
+      });
+    });
+  });
+
+  pollForDone(testCases);
+}
+
+pollForDone(List tests) {
+  if (tests.every((t)=> t.isComplete)) {
+    window.postMessage('done', window.location.href);
+    return;
+  }
+
+  var wait = new Duration(milliseconds: 100);
+  new Timer(wait, ()=> pollForDone(tests));
+}
--- test/test_runner.sh
+++ test/test_runner.sh
@@ -6,13 +6,17 @@ if [[ $? != 0 ]]
   then exit 1
 fi
 
-# Run a set of Dart Unit tests
-results=$(content_shell --dump-render-tree test/index.html)
-echo -e "$results"
+# Run different test contexts
+for X in index html5
+do
+  # Run a set of Dart Unit tests
+  results=$(content_shell --dump-render-tree test/$X.html)
+  echo -e "$results"
 
-# check to see if DumpRenderTree tests
-# fails, since it always returns 0
-if [[ "$results" == *"Some tests failed"* ]]
-then
-  exit 1
-fi
+  # check to see if DumpRenderTree tests
+  # fails, since it always returns 0
+  if [[ "$results" == *"Some tests failed"* ]]
+  then
+      exit 1
+  fi
+done
