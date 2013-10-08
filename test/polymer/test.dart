9f3ead6cd2676d74ba624ea0e8fcd699c0020e5d
Chris Strom <github@eeecomputes.com>
2013-10-08 18:07:17 -0400

    Tests for multiple elements

--- test/polymer/test.dart
+++ test/polymer/test.dart
@@ -1,14 +1,16 @@
-library polymer_test;
+library ice_polymer_test;
 
-import 'package:unittest/unittest.dart';
+import 'package:scheduled_test/scheduled_test.dart';
 import 'dart:html';
 import 'dart:async';
+import 'package:polymer/polymer.dart';
+import 'package:ice_code_editor/ice.dart';
 
 main() {
   setUp((){
     var ready = new Completer();
     new Timer(
-      new Duration(milliseconds: 250),
+      new Duration(milliseconds: 5),
       ready.complete
     );
     return ready.future;
@@ -18,7 +20,7 @@ main() {
     test("can embed code", (){
       expect(
         query('ice-code-editor').shadowRoot.query('h1').text,
-        contains('embed.html')
+        contains('embed_foo.html')
       );
     });
     test("can set line number", (){
@@ -38,6 +40,37 @@ main() {
         query('ice-code-editor').query('.ice-code-editor-editor'),
         isNotNull
       );
+    });
+
+    group("multiple elements", (){
+      test("can embed code", (){
+        expect(
+          queryAll('ice-code-editor').last.shadowRoot.query('h1').text,
+          contains('embed_bar.html')
+        );
+      });
+
+      test("can still embed code after JS is loaded and evaluated", (){
+        schedule(()=> Editor.jsReady);
+
+        schedule((){
+          var later = createElement('ice-code-editor')
+            ..xtag.src = 'embed_baz.html';
+
+          document.body.append(later);
+
+          return new Future.delayed(Duration.ZERO);
+        });
+
+        schedule((){
+          expect(
+            queryAll('ice-code-editor').last.shadowRoot.query('h1').text,
+            contains('embed_baz.html')
+          );
+        });
+      });
+
+
     });
   });
 
