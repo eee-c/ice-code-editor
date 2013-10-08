684128f6f05dbbf7586ab308a71fd7f17cd965a7
Chris Strom <github@eeecomputes.com>
2013-10-08 18:06:33 -0400

    Add 2 more polymer elements to the sample page

--- web/polymer.dart
+++ web/polymer.dart
@@ -1,5 +1,18 @@
+import 'dart:html';
 import 'package:polymer/polymer.dart';
 
-@CustomTag('ice-code-editor')
-class IceCodeEditorElement extends PolymerElement {
+main() {
+  // This gets: Removing disallowed element <ICE-CODE-EDITOR> ...
+  // var later = createElementFromHtml('<ice-code-editor src="embed_baz.html" line_number="0"></ice-code-editor>');
+
+  // Don't see attributes change ...
+  // var later = createElement('ice-code-editor')
+  //   ..attributes['src'] = 'embed_baz.html'
+  //   ..attributes['line_number'] = '0';
+
+  var later = createElement('ice-code-editor');
+  var iceElement = later.xtag;
+  iceElement.src = 'embed_c.html';
+
+  document.body.append(later);
 }
