d380977bfbfa7e1db5dd46a714f48906ec9bf59e
Chris Strom <github@eeecomputes.com>
2014-01-19 22:49:40 -0500

    Need to download content_shell in order to run tests.

--- test/test_runner.sh
+++ test/test_runner.sh
@@ -20,6 +20,15 @@ then
 fi
 echo
 
+which content_shell
+if [[ $? -ne 0 ]]; then
+  $DART_SDK/../chromium/download_contentshell.sh
+  unzip content_shell-linux-x64-release.zip
+
+  cs_path=$(ls -d drt-*)
+  PATH=$cs_path:$PATH
+fi
+
 # Run different test contexts
 for X in index html5
 do
