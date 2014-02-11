18fe21acc7425e83fb7d4b012672b10aa07b0d08
Chris Strom <github@eeecomputes.com>
2014-02-10 23:43:42 -0500

    Spike a something's new indicator for main menu button
    Paired with @alexgchancon
    
    #73

--- lib/full.dart
+++ lib/full.dart
@@ -148,11 +148,20 @@ changed.''';
 
   Element _main_menu_button;
   get _mainMenuButton {
+    var new_indicator = new Element.html('<span>★</span>')
+      ..style.color = 'red'
+      ..style.fontSize = '24px'
+      ..style.position = 'absolute'
+      ..style.top = '3px'
+      ..style.right = '-5px'
+      ..style.textShadow = '2px 2px 2px #000000';
+
     return _main_menu_button = new Element.html('<button>☰</button>')
       ..onClick.listen((e) {
         this.toggleMainMenu();
         e.stopPropagation();
-      });
+      })
+    ..append(new_indicator);
   }
   _toggleAutoupdate(CheckboxInputElement e){
     ice.autoupdate = e.checked;
