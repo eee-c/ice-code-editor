2cdfee4d39a0f32b87d68618d945217218e7a6d7
James Hurford <terrasea@gmail.com>
2014-07-13 15:34:29 +1200

    Fixed tests
    Needed to manually call initPolymer() and remove the annotation

--- test/polymer/test.dart
+++ test/polymer/test.dart
@@ -6,7 +6,6 @@ import 'dart:async';
 import 'package:polymer/polymer.dart';
 import 'package:ice_code_editor/ice.dart';
 
-//
 class PageComponent {
   final PolymerElement el;
 
@@ -20,50 +19,49 @@ class PageComponent {
   }
 }
 
-@initMethod
+createElement(String html) =>
+  new Element.html(html, treeSanitizer: new NullTreeSanitizer());
+
+class NullTreeSanitizer implements NodeTreeSanitizer {
+  void sanitizeTree(node) {}
+}
+
 main() {
   var component1;
   var component2;
-  //initPolymer();
+  initPolymer();
 
   setUp((){
-    //@TODO: check to see if async is returning or not
     schedule(() => Polymer.onReady );
-    print("Entered Setup");
 
     schedule(() {
-      print("Setup schedule");
       var elements = queryAll('ice-code-editor');
 
       component1 = new PageComponent(elements[0]);
       component2 = new PageComponent(elements[1]);
-
-      //@TODO: investigate better async delay support for init of polymer element before test
 
       return Future.wait([component1.flush(), component2.flush()]);
     });
   });
 
   group("[polymer]", (){
-    solo_test("can embed code", (){
-      print("Entered test");
+    test("can embed code", (){
       schedule(() {
-        print("in test schedule");
         expect(
-          query('ice-code-editor').shadowRoot.query('h1').text,
+          query('ice-code-editor').shadowRoot.query('p').text,
           contains('embed_foo.html')
         );
       });
     });
     test("can set line number", (){
       expect(
-        query('ice-code-editor').shadowRoot.query('h1').text,
-        contains('(42)')
+        query('ice-code-editor').shadowRoot.query('p').text,
+        contains('42')
       );
     });
     test("creates a shadow preview", (){
       expect(
-        query('ice-code-editor').shadowRoot.query('.ice-code-editor-preview'),
+        query('ice-code-editor').query('.ice-code-editor-preview'),
         isNotNull
       );
     });
@@ -77,7 +75,7 @@ main() {
     group("multiple elements", (){
       test("can embed code", (){
         expect(
-          queryAll('ice-code-editor').last.shadowRoot.query('h1').text,
+          queryAll('ice-code-editor').last.shadowRoot.query('p').text,
           contains('embed_bar.html')
         );
       });
@@ -86,17 +84,16 @@ main() {
         schedule(()=> Editor.jsReady);
 
         schedule((){
-          var later = createElement('ice-code-editor')
-            ..xtag.src = 'embed_baz.html';
-
+          var later = createElement('<ice-code-editor src="embed_baz.html"></ice-code-editor>');
           document.body.append(later);
+          currentSchedule.onComplete.schedule(() => later.remove());
 
           return new Future.delayed(Duration.ZERO);
         });
 
         schedule((){
           expect(
-            queryAll('ice-code-editor').last.shadowRoot.query('h1').text,
+            queryAll('ice-code-editor').last.shadowRoot.query('p').text,
             contains('embed_baz.html')
           );
         });
