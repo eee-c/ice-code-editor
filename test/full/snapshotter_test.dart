42ae41878b2b0a7467c1c5b9c710dc24a70c49cb
Chris Strom <github@eeecomputes.com>
2014-09-06 23:45:01 -0400

    Handle case of snapshotting a now active, previously snapshot project
    Paired with @terrasea
    
    #87

--- test/full/snapshotter_test.dart
+++ test/full/snapshotter_test.dart
@@ -1,7 +1,7 @@
 part of ice_test;
 
 snapshotter_tests() {
-  group('Snapshotter', () {
+  solo_group('Snapshotter', () {
     var editor;
 
     setUp((){
@@ -56,9 +56,21 @@ snapshotter_tests() {
       expect(snapshot, isNull);
     });
 
+    test('snapshotting an old snapshot that is now active, strips SNAPSHOT from title', (){
+      editor.store['SNAPSHOT: Saved Project (2014-09-06 23:33)'] = {
+        'code': 'old snapshot',
+        'snapshot': false
+      };
+
+      expect(editor.store.currentProject['code'], 'old snapshot');
+
+      editor.snapshotter.take();
+      var snapshot = editor.store['SNAPSHOT: Saved Project (${Snapshotter.dateStr})'];
+      expect(snapshot['snapshot'], isTrue);
+    });
+
     // QUESTION: should we add a test for the update-by-reference bug we ran into last time?
     // QUESTION: When someone opens a Snapshot, what happens to the flag?
-
     //TODO: after ?s, the last project should be opened, *not* the most recent snapshot
   });
 }
