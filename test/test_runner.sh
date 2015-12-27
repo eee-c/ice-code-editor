4fc0f38f06d40685d77eea315abbf704b06fc5c0
Chris Strom <github@eeecomputes.com>
2015-12-27 14:05:29 -0500

    Dart analyzer output is now one fewer lines
    Paired with @terrasea
    
    #102

--- test/test_runner.sh
+++ test/test_runner.sh
@@ -10,7 +10,7 @@ results_ignoring_ok_deprecations=$(
 )
 echo "$results_ignoring_ok_deprecations"
 count=$(echo "$results_ignoring_ok_deprecations" | wc -l)
-if [[ "$count" != "2" ]]
+if [[ "$count" != "1" ]]
 then
   exit 1
 fi
